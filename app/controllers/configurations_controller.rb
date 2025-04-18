3993553164e979c1663d80d86fd9acc2e3854a9e
Chris Arndt <cgarndt@wisc.edu>
2025-04-18 09:14:03 -0500

    Ch. 3 completed with a few bugs but the gist is there.

---
+++ app/controllers/configurations_controller.rb
@@ -0,0 +1,49 @@
+class ConfigurationsController < ApplicationController
+
+
+  def ios_v1
+    render json: {
+      settings: {},
+      rules: [
+        {
+          patterns: [
+            "/new$",
+            "/edit$"
+          ],
+          properties: {
+            context: "model"
+          }
+        }
+      ]
+    }
+  end
+
+  def android_v1
+    render json: {
+      settings: {},
+      rules: [
+        {
+          patterns: [
+            ".*"
+          ],
+          properties: {
+            uri: "hotwire://fragment/web",
+            pull_to_refresh_enabled: true
+          }
+        },
+        {
+          patterns: [
+            "/new$",
+            "/edit$"
+          ],
+          properties: {
+            context: "model",
+            pull_to_refresh_enabled: false
+          }
+        }
+      ]
+    }
+  end
+
+
+end # END class
